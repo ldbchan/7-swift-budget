cec5530444cd3bf12c6e4a5da727b118b192e656
ldbchan <ldb.chan@gmail.com>
2023-03-15 16:20:52 +0800

    Add project

---
+++ SwiftBudge/SwiftBudge/SwiftBudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBudgeApp.swift
+//  SwiftBudge
+//
+//  Created by chantil on 2023/3/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
